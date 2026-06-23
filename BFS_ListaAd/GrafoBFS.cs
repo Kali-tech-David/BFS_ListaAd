ad3ee71c1efeed16f5dac6f1bad0830b8e60d9a8
Kali-Tech-David <david.aguirreks@gmail.com>
2026-06-23 12:15:31 -0600

    Agregar archivos de proyecto.

---
+++ BFS_ListaAd/GrafoBFS.cs
@@ -0,0 +1,61 @@
+﻿using BSF_ListaAd;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFS_ListaAd
+{
+    internal class GrafoBFS
+    {
+        private int v;
+        private ListaEnlazadaDoble[] adj;
+
+        public GrafoBFS(int v)
+        {
+            this.v = v;
+            adj = new ListaEnlazadaDoble[v];
+            for(int i = 0; i< adj.Length; i++)
+            {
+                adj[i] = new ListaEnlazadaDoble();
+            }
+        }
+
+        public void AgregarArista(int v, int w)
+        {
+            adj[v].AgregaFin(w);
+        }
+
+
+        public void BFS(int inicio)
+        {
+            bool[] visitado = new bool[v];
+
+            Cola cola = new Cola();
+
+            visitado[inicio] = true;
+            cola.Encolar(inicio);
+
+            while (!cola.EstaVacia())
+            {
+                int actual = cola.Desencolar();
+
+                Console.Write(actual + " ");
+
+                ListaEnlazadaDoble vecinos = adj[actual];
+
+                for (int i = 0; i < vecinos.Length(); i++)
+                {
+                    int vecino = vecinos.Get(i);
+
+                    if (!visitado[vecino])
+                    {
+                        visitado[vecino] = true;
+                        cola.Encolar(vecino);
+                    }
+                }
+            }
+        }
+    }   
+}
